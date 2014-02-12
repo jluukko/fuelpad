63d527a15e3b6ab9437b41a8ca0edaf32eeb1f8c
Julius Luukko <julle@tuutikki.(none)>
2014-02-12 16:17:49 +0200

    Add class PlotDataModel

---
+++ plotdatamodel.cpp
@@ -0,0 +1,114 @@
+/*
+ * This file is part of Fuelpad.
+ *
+ * Copyright (C) 2007-2012,2014 Julius Luukko <julle.luukko@quicknet.inet.fi>
+ *
+ * Fuelpad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fuelpad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fuelpad.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "plotdatamodel.h"
+
+PlotData::PlotData(const double xc, const double yc)
+{
+    m_xc = xc;
+    m_yc = yc;
+}
+
+QVariant PlotData::data(int role) const
+{
+    switch (role) {
+    case xRole:
+        return m_xc;
+    case yRole:
+        return m_yc;
+    default:
+        return 0;
+    }
+}
+
+double PlotData::xc(void) const
+{
+    return m_xc;
+}
+
+double PlotData::yc(void) const
+{
+    return m_yc;
+}
+
+PlotDataModel::PlotDataModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+    QHash<int, QByteArray> roleNames;
+    roleNames[PlotData::xRole] =  "xc";
+    roleNames[PlotData::yRole] = "yc";
+
+    setRoleNames(roleNames);
+}
+
+PlotDataModel::~PlotDataModel(void)
+{
+}
+
+int PlotDataModel::count(void) const
+{
+    return rowCount();
+}
+
+int PlotDataModel::rowCount ( const QModelIndex & parent ) const
+{
+    return m_data.count();
+}
+
+void PlotDataModel::appendData(const PlotData &item)
+{
+    beginInsertRows(QModelIndex(), rowCount(), rowCount());
+    m_data << item;
+    const QModelIndex idx = PlotDataModel::index(m_data.count(), 0);
+    emit dataChanged(idx, idx);
+    endInsertRows();
+}
+
+QVariant PlotDataModel::data(const QModelIndex &index, int role) const
+{
+    if (index.row() < 0 || index.row() > m_data.count()) {
+        return QVariant();
+
+        const PlotData &data = m_data[index.row()];
+        if (role = PlotData::xRole) {
+            return data.xc();
+        }
+        else if (role == PlotData::yRole) {
+            return data.yc();
+        }
+        return QVariant();
+    }
+}
+
+QVariant PlotDataModel::get(int index) const
+{
+    const PlotData &item = m_data.at(index);
+    QMap<QString, QVariant> itemData;
+    QHashIterator<int, QByteArray> hashItr(roleNames());
+    while(hashItr.hasNext()){
+        hashItr.next();
+        itemData.insert(hashItr.value(),item.data(hashItr.key()).toString());
+    }
+    return QVariant(itemData);
+}
+
+void PlotDataModel::clear(void) {
+    m_data.clear();
+}
---
+++ plotdatamodel.h
@@ -0,0 +1,77 @@
+/*
+ * This file is part of Fuelpad.
+ *
+ * Copyright (C) 2007-2012,2014 Julius Luukko <julle.luukko@quicknet.inet.fi>
+ *
+ * Fuelpad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fuelpad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fuelpad.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PLOTDATAMODEL_H
+#define PLOTDATAMODEL_H
+
+#include <QStandardItemModel>
+#include <QStandardItem>
+
+class PlotData
+{
+public:
+    enum RoleNames {
+        xRole = Qt::UserRole + 1,
+        yRole
+    };
+    PlotData(const double xc, const double yc);
+
+    QVariant data(int role) const;
+    QHash<int, QByteArray> roleNames() const;
+
+    Q_INVOKABLE double xc(void) const;
+    Q_INVOKABLE double yc(void) const;
+
+private:
+    double m_xc;
+    double m_yc;
+};
+
+class PlotDataModel : public QAbstractListModel
+{
+    Q_OBJECT
+    Q_PROPERTY(int count READ count NOTIFY countChanged)
+//    Q_PROPERTY(QVariant get READ get)
+
+public:
+
+    PlotDataModel(QObject *parent = 0);
+    virtual ~PlotDataModel(void);
+
+    int count(void) const;
+
+    int rowCount ( const QModelIndex & parent = QModelIndex() ) const;
+
+    void appendData(const PlotData &item);
+
+    Q_INVOKABLE QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+
+    Q_INVOKABLE QVariant get(int index) const;
+
+    void clear(void);
+
+private:
+     QList<PlotData> m_data;
+
+signals:
+    void countChanged();
+};
+
+#endif // PLOTDATAMODEL_H
