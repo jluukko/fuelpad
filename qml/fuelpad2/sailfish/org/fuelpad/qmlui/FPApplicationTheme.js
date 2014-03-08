ad3da982fa18cde1234c9b6051f37142fa000cef
Julius Luukko <julle@tuutikki.(none)>
2014-03-08 15:51:48 +0200

    Add theme definition js to avoid UIConstants.js in platform independent code

---
+++ qml/fuelpad2/sailfish/org/fuelpad/qmlui/FPApplicationTheme.js
@@ -0,0 +1,25 @@
+//.pragma library
+
+.import Sailfish.Silica.theme 1.0 as Theme
+
+var fontFamily = Theme.fontFamily
+var fontFamilyHeading = Theme.fontFamilyHeading
+
+var fontSizeExtraSmall = Theme.fontSizeExtraSmall
+var fontSizeSmall = Theme.fontSizeSmall
+var fontSizeMedium = Theme.fontSizeMedium
+var fontSizeLarge = Theme.fontSizeLarge
+var fontSizeExtraLarge = Theme.fontSizeExtraLarge
+
+var primaryColor = Theme.primaryColor
+var secondaryColor = Theme.secondaryColor
+var highlightColor = Theme.highlightColor
+var secondaryHighlightColor = Theme.secondaryHighlightColor
+
+var itemSizeSmall = Theme.itemSizeSmall
+var itemSizeMedium = Theme.itemSizeMedium
+var itemSizeLarge = Theme.itemSizeLarge
+var itemSizeExtraLarge = Theme.itemSizeExtraLarge
+var paddingSmall = Theme.paddingSmall
+var paddingMedium = Theme.paddingMedium
+var paddingLarge = Theme.paddingLarge
